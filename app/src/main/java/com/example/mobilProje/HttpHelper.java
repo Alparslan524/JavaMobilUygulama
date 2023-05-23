a53ab6fca79c3112b9c68611eb4c2c4cb3cf8159
Alparslan <alparslanaydgn02@gmail.com>
2023-05-23 14:14:28 +0300

    Foreign currency process completed

---
+++ app/src/main/java/com/example/mobilProje/HttpHelper.java
@@ -0,0 +1,32 @@
+package com.example.mobilProje;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpHelper {
+    public static String sendGetRequest(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            InputStream inputStream = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            reader.close();
+            return response.toString();
+        } else {
+            throw new IOException("HTTP GET Request Failed with Error code: " + responseCode);
+        }
+    }
+}
+
--- app/src/main/java/com/example/mobilProje/MainActivity3.java
+++ app/src/main/java/com/example/mobilProje/MainActivity3.java
@@ -11,9 +11,19 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    private TextView tvExchangeRateDolar;
+    private TextView tvExchangeRateEuro;
+    private String euroRateStr;
+    private String dolarRateStr;
     Button btn;
     Button btn2;
     EditText h1;
@@ -35,9 +45,15 @@ public class MainActivity3 extends AppCompatActivity {
         rb1= findViewById(R.id.rb1);
         rb2= findViewById(R.id.rb2);
         h1= findViewById(R.id.h1);
-        h2= findViewById(R.id.h2);
         tv= findViewById(R.id.tv);
 
+
+
+        tvExchangeRateDolar = findViewById(R.id.textViewDolar);
+        tvExchangeRateEuro = findViewById(R.id.textViewEuro);
+        // Döviz kuru almak için AsyncTask'i başlat
+        FetchExchangeRateTask task = new FetchExchangeRateTask();
+        task.execute();
 
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -47,7 +63,7 @@ public class MainActivity3 extends AppCompatActivity {
                 if(rb1.isChecked()) {
                     double a, b, c;
                     a = Double.parseDouble(h1.getText().toString());
-                    b = Double.parseDouble(h2.getText().toString());
+                    b = Double.parseDouble(euroRateStr.split(":")[1].trim());
                     c = a * b;
 
                     tv.setText(String.valueOf(c));
@@ -56,7 +72,7 @@ public class MainActivity3 extends AppCompatActivity {
                 if(rb2.isChecked()) {
                     double a, b, c;
                     a = Double.parseDouble(h1.getText().toString());
-                    b = Double.parseDouble(h2.getText().toString());
+                    b = Double.parseDouble(dolarRateStr.split(":")[1].trim());
                     c = a * b;
 
                     tv.setText(String.valueOf(c));
@@ -76,4 +92,48 @@ public class MainActivity3 extends AppCompatActivity {
         });
 
     }
+
+    private class FetchExchangeRateTask extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                // TCMB Döviz Kuru API'sinden verileri almak için HTTP isteği yap
+                String apiUrl = "https://api.exchangerate-api.com/v4/latest/TRY";
+                return HttpHelper.sendGetRequest(apiUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+
+            if (response != null) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    JSONObject rates = jsonObject.getJSONObject("rates");
+
+                    // TL/Dolar kurunu al
+                    double tryToUsd = 1 / rates.getDouble("USD"); // Burada 1'e bölerek TL/Dolar kurunu elde ediyoruz
+                    double tryToEuro = 1/rates.getDouble("EUR");
+                    tvExchangeRateDolar.setText("1 Dolar: " + tryToUsd);
+                    tvExchangeRateEuro.setText("1 Euro: "+tryToEuro);
+                    euroRateStr="1 Euro: " + tryToEuro;
+                    dolarRateStr="1 Dolar: " + tryToUsd;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    tvExchangeRateDolar.setText("Kur alınamadı.");
+                    tvExchangeRateEuro.setText("Kur alınamadı.");
+                }
+            } else {
+                tvExchangeRateDolar.setText("Kur alınamadı.");
+                tvExchangeRateEuro.setText("Kur alınamadı.");
+            }
+        }
+    }
+
+
+
 }
